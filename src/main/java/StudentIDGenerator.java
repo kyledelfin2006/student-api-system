491145af04711669fd5694bab50e7321c5cf6312
kyledelfin2006 <aldrinkyles@1219@gmail.com>
2026-04-05 03:22:00 +0800

    Made StudentIDGenerator Thread Safe using Atomic Integer

--- src/main/java/StudentIDGenerator.java
+++ src/main/java/StudentIDGenerator.java
@@ -1,14 +1,16 @@
-public class StudentIDGenerator {
-    private static int nextId;
+import java.util.concurrent.atomic.AtomicInteger;
 
-    public static String generateNextID(){
-        String id = String.format("%04d", nextId);
-        nextId++;
-        return id; // "0001", "0002", etc.
+public class StudentIDGenerator {
+    private static final AtomicInteger nextId = new AtomicInteger(0);
+
+    public static String generateNextID() {
+        int id = nextId.getAndIncrement();  // READ + WRITE as ONE operation
+        return String.format("%04d", id);
     }
 
 
-    public static void setNextId(int nextId) {
-        StudentIDGenerator.nextId = nextId;
+    // Setter for NextId
+    public static void setNextId(int id) {
+        StudentIDGenerator.nextId.set(id);
     }
 }
